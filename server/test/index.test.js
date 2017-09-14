f4e190bed9f260c332b1bc53e23a623e70c66192
Bill Feng <mr.billfeng@gmail.com>
2017-09-14 17:34:21 +0900

    Initial commit

---
+++ server/test/index.test.js
@@ -0,0 +1,19 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+
+const app = require('../app');
+
+chai.use(chaiHttp);
+
+describe('Index route', () => {
+  it('should display a saxophone.', done => {
+    chai.request(app)
+      .get('/')
+      .end((err, res) => {
+        chai.expect(res).to.have.status(200);
+        chai.expect(res).to.have.header('content-type', 'text/html; charset=utf-8');
+        chai.expect(res.text).contains('sax2.png');
+        done();
+      })
+  });
+});
