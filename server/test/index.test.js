cee51d72498385dedbeaf6d375200a56f899710f
Bill Feng <mr.billfeng@gmail.com>
2017-09-14 18:19:08 +0900

    Test passing

--- server/test/index.test.js
+++ server/test/index.test.js
@@ -14,7 +14,7 @@ describe('Index route', () => {
       .end((err, res) => {
         chai.expect(res).to.have.status(200);
         chai.expect(res).to.have.header('content-type', 'text/html; charset=utf-8');
-        chai.expect(res.text).contains('sax2.png');
+        chai.expect(res.text).contains('sax.png');
         done();
       });
   });
