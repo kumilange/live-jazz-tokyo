ed2ea7cbc7f13d1a86db773714f2329f827e0527
Kumiko Haraguchi <email.935@gmail.com>
2017-09-15 18:09:44 +0900

    Populate event_img data

---
+++ server/scripts/populate-event-img.js
@@ -0,0 +1,26 @@
+/* eslint-disable no-console */
+
+const fs = require('fs');
+const knex = require('knex');
+const knexConfig = require('../knexfile');
+const db = knex(knexConfig);
+
+const filenames = fs.readdirSync('../data/img');
+const promises = filenames.map(async (file) => {
+  try {
+    const image = fs.readFileSync(`../data/img/${file}`).toString('base64');
+    const id = await db('event_img')
+      .insert({ image })
+      .returning('id');
+    console.log(`${image} inserted with id ${id}`);
+  } catch (err) {
+    console.error(`Inserting image failed!\n${err}`);
+  }
+});
+
+Promise.all(promises)
+  .then(() => {
+    console.log('All done!');
+    process.exit();
+  });
+
