577b0fc7dfe355f3e3320dc7014c462d7d1a0b0c
Michael Sodaro <xtra.zero@gmail.com>
2017-09-15 10:08:26 +0900

    Serve frontend statically on express

--- server/app.js
+++ server/app.js
@@ -19,7 +19,8 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
+
+app.use('/', express.static('build'));
 app.use('/users', users);
 
 // catch 404 and forward to error handler
