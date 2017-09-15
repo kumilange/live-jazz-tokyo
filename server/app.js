a9279fa6a827f73322646e9414a1b79e8f65f4d8
Michael Sodaro <xtra.zero@gmail.com>
2017-09-15 11:59:41 +0900

    Fixed feedback from PR

--- server/app.js
+++ server/app.js
@@ -8,10 +8,6 @@ const users = require('./routes/users');
 
 const app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
