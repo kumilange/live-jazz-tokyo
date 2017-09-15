540f5aa08120e9d68bf49ea930b6d848d66cd949
Kumiko Haraguchi <email.935@gmail.com>
2017-09-15 14:57:31 +0900

    Migrations working

--- server/knexfile.js
+++ server/knexfile.js
@@ -1,9 +1,9 @@
 const knex = require('knex');
 
-const db = knex({
+const db = {
   client: 'pg',
   connection: process.env.DATABASE_URL || `postgres://${process.env.USER}@127.0.0.1:5432/livejazz`,
   searchPath: 'public',
-});
+};
 
 module.exports = db;
--- server/migrations/201709151402_event.js
+++ server/migrations/201709151402_event.js
@@ -2,11 +2,11 @@ const setup = knex =>
   knex.schema.createTable('event', (t) => {
     t.increments().index();
     t.integer('artist_id').unsigned();
-    t.forerign('artist_id').references(artist.id);
+    t.foreign('artist_id').references('artist.id');
     t.integer('venue_id').unsigned();
-    t.forerign('venue_id').references(venue.id);
+    t.foreign('venue_id').references('venue.id');
     t.integer('event_image_id').unsigned();
-    t.forerign('event_image_id').references(event_img.id);
+    t.foreign('event_image_id').references('event_img.id');
     t.integer('price');
     t.timestamp('start_time');
     t.timestamp('end_time');
