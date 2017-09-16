14f211efa02cc5edd148df562186a2422befbd1f
Michael Sodaro <xtra.zero@gmail.com>
2017-09-16 09:13:17 +0900

    Pins show, but stack and don't auto-close

--- src/Map.js
+++ src/Map.js
@@ -1,29 +1,60 @@
 import React, { Component } from 'react';
-import { withGoogleMap, GoogleMap, Marker } from 'react-google-maps';
+import { InfoWindow, withGoogleMap, GoogleMap, Marker } from 'react-google-maps';
 import PropTypes from 'prop-types';
+
+class MyMarker extends Component {
+  state = {
+    isOpen: false
+  }
+
+  render() {
+    const position = {
+      lat: this.props.marker.lat,
+      lng: this.props.marker.lng,
+    };
+    
+    return <Marker
+      id='marker'
+      position={position}
+      key={String(position.lat) + String(position.lng)}
+      title={this.props.marker.event}
+      onClick={ () => {
+        if(!this.state.isOpen) {
+          this.setState({
+            isOpen: true
+          });
+        }
+      }}
+    >
+      { this.state.isOpen &&
+        <InfoWindow
+          ref={ node => this.infoWindowRef = node }
+          onCloseClick={ () => {
+            this.setState({
+              isOpen: false
+            });
+        }} >
+          <div>
+            TEST
+          </div>
+        </InfoWindow>
+      }
+    </Marker>
+  }
+}
 
 const MyMap = withGoogleMap(props => (
   <GoogleMap
     defaultZoom={14}
     defaultCenter={{ lat: 35.6857933, lng: 139.7501793 }}
-    onClick={props.onMapClick}
   >
-    {props.markers.map((marker) => {
-      const position = {
-        lat: marker.lat,
-        lng: marker.lng,
-      };
-      return (<Marker
-        position={position}
-        key={String(position.lat) + String(position.lng)}
-        title={marker.event}
-      />);
-    })}
+    { props.markers.map( (marker) => <MyMarker marker={ marker } /> ) }
   </GoogleMap>
 ));
 
 class Map extends Component {
   render() {
+    console.log('MAP');
     return process.env.npm_lifecycle_event === 'test' ? <div /> : <MyMap
       markers={this.props.markers}
       className="test"
