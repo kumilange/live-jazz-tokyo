a6edc8b6addeebac67579d3b9399e6758aeaa927
Michael Sodaro <xtra.zero@gmail.com>
2017-09-18 12:02:18 +0900

    Set up react-router

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
+import { BrowserRouter, Route, Link } from 'react-router-dom'
 
 import reducer from './reducers';
 import App from './containers/App';
@@ -10,7 +11,11 @@ import './styles/index.css';
 const store = createStore(reducer);
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <BrowserRouter>
+      <div id="router">
+        <Route path="/" component={App} />
+      </div>
+    </BrowserRouter>
   </Provider>,
   document.getElementById('root'),
 );
