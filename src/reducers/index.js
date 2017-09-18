4f91891e737f1b907ada50e29bdc70057bfdad0e
Michael Sodaro <xtra.zero@gmail.com>
2017-09-18 09:58:13 +0900

    Log error for unknown state

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -12,6 +12,7 @@ const reducer = (state = initialState, action) => {
     case 'SET_SELECTED_EVENT':
       return Object.assign({}, state, { selectedEvent: action.event });
     default:
+      console.log('UNKNOWN STATE!');
       return state;
   }
 };
