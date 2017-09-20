ff03c2d385f61ab42e33f44d24f3bc3e1c332934
Bill Feng <mr.billfeng@gmail.com>
2017-09-20 16:19:36 +0900

    Basic header, footer, and css

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Footer = () => (
+  <footer className="flex center">
+    <p>Live Jazz © 2017</p>
+  </footer>
+)
+
+export default Footer;
--- src/index.js
+++ src/index.js
@@ -6,16 +6,22 @@ import { BrowserRouter, Route } from 'react-router-dom';
 
 import './styles/index.css';
 import reducer from './reducers';
+import Header from './components/Header';
 import App from './containers/App';
 import Event from './containers/Event';
+import Footer from './components/Footer';
 
 const store = createStore(reducer);
 ReactDOM.render(
   <Provider store={store}>
     <BrowserRouter>
-      <div id="router">
-        <Route exact path="/" component={App} />
-        <Route exact path="/event/:id" component={Event} />
+      <div id="router" className="flex column">
+        <Header />
+        <main className="grow">
+          <Route exact path="/" component={App} />
+          <Route exact path="/event/:id" component={Event} />
+        </main>
+        <Footer />
       </div>
     </BrowserRouter>
   </Provider>,
