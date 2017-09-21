bc2540497f109cca24b739206605e92e8a55a9a2
Kumiko Haraguchi <email.935@gmail.com>
2017-09-21 10:36:05 +0900

    Fix linter errors

--- server/test/init.js
+++ server/test/init.js
@@ -2,13 +2,9 @@ const knex = require('knex');
 const config = require('../../knexfile');
 
 // HACK - work around for heroku being broken
-console.log(`DATABASE_URL=`, DATABASE_URL);
 Object.keys(process.env).forEach((key) => {
   if (key.startsWith('HEROKU_POSTGRESQL')) {
-    const NEWURL = process.env[key];
-    console.log(`${key}=`, NEWURL);
-    process.env.DATABASE_URL = NEWURL;
-    console.log(`new DATABASE_URL=`, process.env.DATABASE_URL);
+    config.connection = process.env[key];
   }
 });
 // END HACK
