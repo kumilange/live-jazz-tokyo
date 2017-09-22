47f8563626ab8749f1cfc1b3e12de541d8a54c85
Kumiko Haraguchi <email.935@gmail.com>
2017-09-22 15:20:59 +0900

    Modify migrations to make names unique

--- migrations/201709151215_artist.js
+++ migrations/201709151215_artist.js
@@ -1,7 +1,7 @@
 const setup = knex =>
   knex.schema.createTable('artist', (t) => {
     t.increments().index();
-    t.string('name').notNullable();
+    t.string('name').notNullable().unique();
     t.text('image');
   });
 
--- migrations/201709151221_venue.js
+++ migrations/201709151221_venue.js
@@ -1,7 +1,7 @@
 const setup = knex =>
   knex.schema.createTable('venue', (t) => {
     t.increments().index();
-    t.string('name').notNullable();
+    t.string('name').notNullable().unique();
     t.decimal('lat', 12, 8).notNullable();
     t.decimal('lng', 12, 8).notNullable();
     t.string('address');
