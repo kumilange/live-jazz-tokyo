3c67bb4bcecd5a8cde51e849343a873f923b1542
haraKumi <email.935@gmail.com>
2017-09-23 14:34:34 +0900

    Fix footer bug

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import '../styles/Header.css';
 
 const Header = () => (
   <header id="header" className="flex dark">
-    <h1><a href="" className="logo">Live Jazz</a></h1>
+    <h1><Link to="/" className="logo">Live Jazz</Link></h1>
     <nav className="gnav">
       <ul className="gnav-list">
         <li><a href="" className="gnav-list-link">Log In</a></li>
