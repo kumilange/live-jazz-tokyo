b48aba91a959b5ca4d4f90bfbc0b4ade290acd3f
Bill Feng <mr.billfeng@gmail.com>
2017-09-23 13:00:35 +0900

    Switch away from table element

--- src/components/Event.js
+++ src/components/Event.js
@@ -2,6 +2,10 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import Stripe from 'react-stripe-checkout';
 import Divider from 'material-ui/Divider';
+import Paper from 'material-ui/Paper';
+import RaisedButton from 'material-ui/RaisedButton';
+
+import Map from '../containers/Map';
 
 import '../styles/Event.css';
 import { ClockIcon, DollarIcon, PinIcon } from '../styles/Icons';
@@ -53,57 +57,72 @@ class Event extends Component {
 
           <Divider />
 
-          <table className="event-details-table">
-            <tbody>
-              <tr>
-                <td><ClockIcon /></td>
-                <td>
-                  <p>{ this.props.event.start.toDateString() }</p>
-                  <p>{
-                    `${this.props.event.start.toTimeString().split(':').slice(0, 2).join(':')
-                    } to ${
-                      this.props.event.end.toTimeString().split(':').slice(0, 2).join(':')}`}
-                  </p>
-                </td>
-              </tr>
-              <tr>
-                <td><PinIcon /></td>
-                <td className="flex center">
-                  <div>
-                    <p>{ this.props.event.venue }</p>
-                    <p>{ this.props.event.address }</p>
-                  </div>
-                  <div className="grow" />
-                  <div>
-                    View Map
-                  </div>
-                </td>
-              </tr>
-              <tr>
-                <td><DollarIcon /></td>
-                <td className="flex center">
-                  <p>{ `${this.props.event.price} Yen`}</p>
-                  <div className="grow" />
-                  <Stripe
-                    label={'Reserve'}
-                    token={(token) => { this.onToken(token); }}
-                    stripeKey="pk_test_6pRNASCoBOKtIshFeQd4XMUh"
-                    currency="JPY"
-                    name="Live Jazz Co."
-                    image="../logo.png"
-                    description={`1 ticket for ${this.props.event.name}`}
-                    amount={2000}
-                  />
-                </td>
-              </tr>
-            </tbody>
-          </table>
+          <div className="event-details-table">
+            <div className="flex center row">
+              <div className="icon"><ClockIcon /></div>
+              <div>
+                <p>{ this.props.event.start.toDateString() }</p>
+                <p>{
+                  `${this.props.event.start.toTimeString().split(':').slice(0, 2).join(':')
+                  } to ${
+                    this.props.event.end.toTimeString().split(':').slice(0, 2).join(':')}`}
+                </p>
+              </div>
+              <div className="grow" />
+            </div>
 
+            <div className="flex center row">
+              <div className="icon"><PinIcon /></div>
+              <div>
+                <p>{ this.props.event.venue }</p>
+                <p>{ this.props.event.address }</p>
+              </div>
+              <div className="grow" />
+              <RaisedButton label="View Map" />
+            </div>
 
-          <div>ARTIST { this.props.event.artist }</div>
-
-
-          <div>DESCRIPTION { this.props.event.description }</div>
+            <div>
+              <div style={{ width: "100%;", height: "100px;" }}>
+                <Map />
+              </div>
+            </div>
+            
+            <div className="flex center row">
+              <div className="icon"><DollarIcon /></div>
+              <p>{ `${this.props.event.price} Yen`}</p>
+              <div className="grow" />
+              <Stripe
+                label={'Reserve'}
+                token={(token) => { this.onToken(token); }}
+                stripeKey="pk_test_6pRNASCoBOKtIshFeQd4XMUh"
+                currency="JPY"
+                name="Live Jazz Co."
+                image="../logo.png"
+                description={`1 ticket for ${this.props.event.name}`}
+                amount={2000}
+              />
+            </div>
+          </div>
+          
+          <Paper className="pane">
+            <div className="block">
+              <h3>Details</h3>
+            </div>
+            <Divider />
+            <div className="block">
+              <div>{ this.props.event.description }</div>
+            </div>
+          </Paper>
+          
+          <Paper className="pane">
+            <div className="block">
+              <h3>Artist</h3>
+            </div>
+            <Divider />
+            <div className="block">
+              <p>{ this.props.event.artist }</p>
+            </div>
+          </Paper>
         </main>
       );
     }
