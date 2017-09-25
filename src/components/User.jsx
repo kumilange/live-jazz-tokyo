3e61fb488e0561604432c651f32b965e9fd263b9
Bill Feng <mr.billfeng@gmail.com>
2017-09-25 13:48:04 +0900

    Add default user image

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -7,6 +7,9 @@ class User extends Component {
   render() {
     return (
       <div>
+        <div id="profile-picture">
+          <img src="/default-user.jpg" />
+        </div>
         <div>ID { this.props.match.params.id }</div>
       </div>
     );
