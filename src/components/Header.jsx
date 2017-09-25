87be2506427b22c36feb34d26d2afd3cd875cd37
Michael Sodaro <xtra.zero@gmail.com>
2017-09-25 19:13:58 +0900

    Make Logout button link back to home

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -11,7 +11,7 @@ const Header = (props) => (
       ? <nav className="gnav">
         <ul className="gnav-list flex">
           <li><Link to={'/user'} className="gnav-list-link">{ props.userProfile.name }</Link></li>
-          <li><span className="gnav-list-link" onClick={props.onLogoutButtonClick}>Logout</span></li>
+          <li><Link to={'/'}  className="gnav-list-link" onClick={props.onLogoutButtonClick}>Logout</Link></li>
         </ul>
         </nav>
       : <nav className="gnav">
