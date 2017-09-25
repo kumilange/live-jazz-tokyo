61b69c1e03c3b083950c4fb05cabcab3cc284021
Michael Sodaro <xtra.zero@gmail.com>
2017-09-25 19:55:55 +0900

    Fix migration

--- migrations/20170925105716_create_user_table.js
+++ migrations/20170925105716_create_user_table.js
@@ -1,38 +1,36 @@
 const setup = (knex) => {
-  knex.schema.createTable('user', (table) => {
-    table.increments().index();
-    table.string('name');
-    table.string('email');
-  });
-
-  knex.schema.table('artist', (table) => {
-    table.integer('user_id')
-      .references('user.id');
-  });
-
-  knex.schema.table('transaction', (table) => {
-    table.integer('user_id')
-      .references('user.id')
-      .notNullable();
-  });
-
-  knex.schema.table('venue', (table) => {
-    table.integer('user_id')
-      .references('user.id');
-  });
+  return knex.schema.createTable('user', (table) => {
+    table.increments().index()
+    table.string('name')
+    table.string('email')
+  })
+  .then(knex.schema.table('artist', (table) => {
+  table.integer('user_id')
+    .references('user.id')
+  }))
+  .then(knex.schema.table('transaction', (table) => {
+  table.integer('user_id')
+    .references('user.id')
+    .notNullable();
+  }))
+  .then(knex.schema.table('venue', (table) => {
+  table.integer('user_id')
+    .references('user.id')
+  }))
 };
 
 const rollback = (knex) => {
-  knex.schema.dropTable('user');
-  knex.schema.table('artist', (table) => {
-    table.dropColumn('user_id');
-  });
-  knex.schema.table('transaction', (table) => {
-    table.dropColumn('user_id');
-  });
-  knex.schema.table('venue', (table) => {
-    table.dropColumn('user_id');
-  });
+  return knex.schema.table('artist', (table) => {
+    table.dropColumn('user_id')
+  })
+  .then(knex.schema.table('transaction', (table) => {
+    table.dropColumn('user_id')
+  }))
+  .then(knex.schema.table('venue', (table) => {
+    table.dropColumn('user_id')
+  }))
+  .then(knex.schema.dropTable('user')
+  );
 };
 
 exports.up = setup;
