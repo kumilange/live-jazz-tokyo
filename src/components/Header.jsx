424baa3763fb12886c0a79ebf9358977f57bbbb2
haraKumi <email.935@gmail.com>
2017-09-25 16:31:02 +0900

    Put Header component back to functional component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,39 +1,36 @@
 import React from 'react';
-import { Component } from 'react';
+import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 
 import '../styles/Header.css';
 
-class Header extends Component {
-
-  get authButton() {
-    if(this.props.userProfile) {
-      return (
-        <nav className="gnav">
-          <ul className="gnav-list flex">
-            <li><Link to={} className="gnav-list-link">{ this.props.userProfile.name }</Link></li>
-            <li><span className="gnav-list-link" onClick={this.props.onLogoutButtonClick}>Logout</span></li>
-          </ul>
-        </nav>
-      )
-    }
-    return  (
-      <nav className="gnav">
+const Header = (props) => (
+  <header id="header" className="flex dark">
+    <h1><Link to="/" className="logo">Live Jazz</Link></h1>
+    { props.userProfile
+      ? <nav className="gnav">
         <ul className="gnav-list flex">
-          <li><Link to={} className="gnav-list-link" onClick={this.props.onLoginButtonClick}>Log in with facebook</Link></li>
+          <li><Link to={''} className="gnav-list-link">{ props.userProfile.name }</Link></li>
+          <li><span className="gnav-list-link" onClick={props.onLogoutButtonClick}>Logout</span></li>
+        </ul>
+        </nav>
+      : <nav className="gnav">
+        <ul className="gnav-list flex">
+          <li><Link to={''} className="gnav-list-link" onClick={props.onLoginButtonClick}>Log in with facebook</Link></li>
         </ul>
       </nav>
-      );
-  }
+    }
+  </header>
+)
 
-  render() {
-    return (
-      <header>
-        <h1><Link to="/" className="logo">Live Jazz</Link></h1>
-        { this.authButton }
-      </header>
-    )
-  }
-}
+Header.propTypes = {
+  userProfile: PropTypes.shape(),
+  onLogoutButtonClick: PropTypes.func.isRequired,
+  onLoginButtonClick: PropTypes.func.isRequired,
+};
+
+Header.defaultProps = {
+  userProfile: undefined,
+};
 
 export default Header;
