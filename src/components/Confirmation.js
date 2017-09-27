4f8667d055d6f53d9da7c78cc404da52bc10a2d2
haraKumi <email.935@gmail.com>
2017-09-27 21:40:34 +0900

    Change name

--- src/components/Confirmation.js
+++ src/components/Confirmation.js
@@ -8,7 +8,7 @@ const Confirmation = props => (
   <main id="confirmation" className="restrict-width">
     <div className="flex center">
       <CheckIcon style={{ width: 60, height: 60, paddingRight: 10 }} />
-      <h2 className="confirmTtl">Your reservation is complete!</h2>
+      <h2 className="confirmationTtl">Your reservation is complete!</h2>
       <div className="grow" />
     </div>
     {props.chargeResponse ?
