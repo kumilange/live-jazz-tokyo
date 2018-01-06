5ca5217bf0eb5f89b5502f298acfbe7666291912
Kumiko Haraguchi <email.935@gmail.com>
2018-01-06 17:13:53 +0900

    Add unstaged files

---
+++ src/__tests__/Event.test.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { expect } from 'chai';
+import { render } from 'enzyme';
+
+import Event from '../components/Event';
+
+xit('should have a reserve button', () => {
+  const wrapper = render(<Event
+    event={{}}
+    match={{ params: { id: 1 } }}
+    receivedEventDetails={() => {}}
+    onReserveClicked={() => {}}
+  />);
+  expect(wrapper.find('button.StripeCheckout')).to.have.length(1);
+  expect(wrapper.find('button.StripeCheckout').text()).to.equal('Reserve');
+});
