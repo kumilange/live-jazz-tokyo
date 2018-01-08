4b44d21bc1a3398fdeb9173235f0610c1b89cc18
Kumiko Haraguchi <email.935@gmail.com>
2018-01-08 19:26:47 +0900

    Refactor Header/Auth related components

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,35 +1,44 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 
 import '../styles/Header.css';
 
-const Header = props => (
-  <header className="dark flex">
-    <div id="header-inner" className="flex restrict-width">
-      <h1 className="flex">
-        <Link to="/" className="logo">LiveJazz</Link>
-      </h1>
-      { props.userProfile
-        ? <nav className="gnav">
-          <ul className="gnav-list flex">
-            <li><Link to={'/user'} className="gnav-list-link">{ props.userProfile.name }</Link></li>
-            <li><Link to={'/'} className="gnav-list-link" onClick={props.onLogoutButtonClick}>Logout</Link></li>
-          </ul>
-        </nav>
-        : <nav className="gnav">
-          <ul className="gnav-list flex">
-            <li><Link to={''} className="gnav-list-link" onClick={props.onLoginButtonClick}>
-              Login with <br /> Facebook</Link></li>
-          </ul>
-        </nav>
-      }
-    </div>
-  </header>
-);
+class Header extends Component {
+  componentDidMount() {
+    this.props.onComponentDidMount();
+  }
+  render() {
+    return (
+      <header className="dark flex">
+        <div id="header-inner" className="flex restrict-width">
+          <h1 className="flex">
+            <Link to="/" className="logo">LiveJazz</Link>
+          </h1>
+          { this.props.userProfile
+            ? <nav className="gnav">
+              <ul className="gnav-list flex">
+                <li><Link to={'/user'} className="gnav-list-link">{ this.props.userProfile.name }</Link></li>
+                <li><Link to={'/'} className="gnav-list-link" onClick={this.props.onLogoutButtonClick}>Logout</Link></li>
+              </ul>
+            </nav>
+            : <nav className="gnav">
+              <ul className="gnav-list flex">
+                <li><Link to={''} className="gnav-list-link" onClick={this.props.onLoginButtonClick}>
+                  Login with <br /> Facebook</Link></li>
+              </ul>
+            </nav>
+          }
+        </div>
+      </header>
+    );
+  }
+}
+
 
 Header.propTypes = {
   userProfile: PropTypes.shape(),
+  onComponentDidMount: PropTypes.func.isRequired,
   onLogoutButtonClick: PropTypes.func.isRequired,
   onLoginButtonClick: PropTypes.func.isRequired,
 };
--- src/containers/Header.js
+++ src/containers/Header.js
@@ -1,79 +1,24 @@
 import { connect } from 'react-redux';
-import Header from '../components/Header';
 
-import { setJWT, setUserProfile, logout } from '../actions';
+import { addAuthListener, logout } from '../actions';
+import hello from '../config/hello';
+import Header from '../components/Header';
 
 const mapStateToProps = state => ({
   userProfile: state.userProfile,
 });
 
-// This is where hello.js is used.
-// It is pulled in as a global attached to the window.
-
-// Simplify JSON posting
-const postJson = async (url, objToPost) => {
-  const serializedJson = JSON.stringify(objToPost);
-  const headers = {
-    Accept: 'application/json',
-    'Content-Type': 'application/json',
-  };
-  const options = {
-    headers,
-    method: 'POST',
-    body: serializedJson,
-  };
-  const readableStream = await fetch(url, options);
-  const parsedResponseObject = await (readableStream).json();
-  return parsedResponseObject;
-};
-
 const mapDispatchToProps = (dispatch) => {
-  // Initialize hello with facebook app_id
-
-  window.hello.init({
-    facebook: '1053212748114006',
-  }, {
-    redirect_uri: '/',
-  });
-
-  // Register hello callback once, before anything is dispatched
-  // When hello attempts to log in after login button click, this callback is executed.
-  window.hello.on('auth.login', async (auth) => {
-    const socialToken = auth.authResponse.access_token;
-    window.hello('facebook');
-    // TODO: stop relying on the server to send user's name and email
-    // should only receive JWT
-    const response = await postJson('/api/auth', {
-      network: 'facebook',
-      socialToken,
-    });
-    if (response.userProfile.name) {
-      dispatch(setUserProfile(response.userProfile));
-    }
-    if (response.jwt) {
-      dispatch(setJWT(response.jwt));
-    }
-  });
   return {
-    onLoginButtonClick: () => {
-      const facebook = window.hello('facebook');
-      facebook.login(
-        {
-          scope: 'email',
-        },
-      ).then(() => {
-        return facebook.api('me');
-      }).then((response) => {
-        if (response.userProfile) {
-          dispatch(setUserProfile(response.userProfile));
-        }
-        if (response.jwt) {
-          dispatch(setJWT(response.jwt));
-        }
-      });
+    onComponentDidMount: () => {
+      dispatch(addAuthListener());
+    },
+    onLoginButtonClick: async () => {
+      hello('facebook')
+        .login({ scope: 'email' });
     },
     onLogoutButtonClick: () => {
-      window.hello.logout('facebook');
+      hello.logout('facebook');
       dispatch(logout());
     },
   };
