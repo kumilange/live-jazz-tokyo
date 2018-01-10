6498614c9cd8de17b74b8bd87c3e38150fdd4a15
Kumiko Haraguchi <email.935@gmail.com>
2018-01-10 22:46:20 +0900

    Make error handling simpler

--- server/routes/charge.js
+++ server/routes/charge.js
@@ -15,58 +15,53 @@ const verifyJwt = (jwt) => {
   });
 };
 
-const saveCharge = async (params) => {
-  try {
-    const { charge, userID, eventID } = params;
-    const [result] = await db('transaction')
-      .returning('id')
-      .insert({
-        event_id: eventID,
-        total: charge.amount,
-        charge_id: charge.id,
-        user_id: userID,
-      });
-    console.log('result', result);
-    return {
-      OK: true,
-      order_id: result,
-    };
-  } catch (error) {
-    console.log('err', error);
-    return { error };
-  }
-};
-
-const handleCharge = (params, cb) => {
-  const { price, stripeToken, userID, eventID } = params;
-  stripe.charges.create({
+const createStripeParam = (price, stripeToken) => {
+  return {
     amount: price,
     currency: 'jpy',
     description: 'Example charge',
     source: stripeToken.id,
-  },
-  async (err, charge) => {
-    if (err) {
-      cb(null, err);
-      return;
-    }
-    const result = await saveCharge({ charge, userID, eventID });
-    const { error } = result;
-    console.log('charge result', result);
-    if (error) {
-      cb(null, error);
-      return;
-    }
-    cb(result, null);
-  });
+  };
 };
 
+const createQueryObj = (userID, eventID, charge) => {
+  return {
+    event_id: eventID,
+    total: charge.amount,
+    charge_id: charge.id,
+    user_id: userID,
+  };
+};
+
+const handleCharge = (params, cb) => {
+  const { price, stripeToken, userID, eventID } = params;
+  stripe.charges.create(
+    createStripeParam(price, stripeToken),
+    async (err, charge) => {
+      let result;
+      if (err) {
+        result = { OK: false };
+      } else {
+        const [transactionID] = await db('transaction')
+          .insert(createQueryObj(userID, eventID, charge))
+          .returning('id');
+        console.log('transactionID', transactionID);
+        result = {
+          OK: true,
+          order_id: transactionID,
+        };
+      }
+      cb(result);
+    });
+};
+
+/* GET charge */
 router.get('/', async (req, res) => {
   try {
     console.log('verified jwt', req.headers.bearer);
     const decodedJWT = verifyJwt(req.headers.bearer);
 
-    const result = await db('transaction')
+    const transactions = await db('transaction')
       .leftJoin('user', 'transaction.user_id', 'user.id')
       .leftJoin('event', 'transaction.event_id', 'event.id')
       .where({ email: decodedJWT.email })
@@ -76,46 +71,38 @@ router.get('/', async (req, res) => {
         'event.id as eventId',
         'transaction.total as amount',
       );
-    console.log('result', result);
-    sendResponse(res, RES_STAT.OK.CODE, result);
+    console.log('transactions', transactions);
+    sendResponse(res, RES_STAT.OK.CODE, transactions);
   } catch (err) {
     console.log('err', err);
     sendResponse(res, RES_STAT.INTL_SERVER_ERR.CODE, RES_STAT.INTL_SERVER_ERR.MSG);
   }
 });
 
+/* POST charge */
 router.post('/', async (req, res) => {
   try {
     const { stripeToken, eventID } = req.body;
     console.log('charge jwt', req.headers.bearer);
     const decodedJWT = verifyJwt(req.headers.bearer);
 
-    const userID = (await db('user')
+    // get userID
+    const [{ id }] = await db('user')
       .where({ email: decodedJWT.email })
-      .select('id')
-      .first())
-      .id;
+      .select('id');
+    const userID = id;
     console.log('userID', userID);
 
-    const { price } = await db('event')
+    // get price
+    const [{ price }] = await db('event')
       .select('price')
-      .where('id', eventID)
-      .first();
+      .where('id', eventID);
     console.log('price', price);
 
-    // TODO handle errors properly 
-    if (!(userID && price)) {
-      throw new TypeError('Internal Server Error.');
-    }
-
-    // Charge the user's card:
+    // charge the user's card
     const args = { price, stripeToken, userID, eventID };
-    handleCharge(args, (response, err) => {
-      if (err && !err.OK) {
-        // TODO handle error properly 
-        throw new TypeError('Internal Server Error.');
-      }
-      sendResponse(res, RES_STAT.OK.CODE, response);
+    handleCharge(args, (result) => {
+      sendResponse(res, RES_STAT.OK.CODE, result);
     });
   } catch (err) {
     console.log('err', err);
