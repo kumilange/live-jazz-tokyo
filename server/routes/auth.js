6067304aa24dd4ac0370ab79b1a8555cc9447b00
Kumiko Haraguchi <email.935@gmail.com>
2018-01-10 21:55:34 +0900

    Put back db handling directly in router code

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -17,30 +17,6 @@ const createQueryParam = (socialToken) => {
   return querystring.stringify(params);
 };
 
-const saveNewUser = async (profile) => {
-  try {
-    let user = await db('user')
-      .where({ email: profile.email })
-      .first();
-
-    if (user) {
-      console.log('existing user', user);
-    } else {
-      [user] = await db('user')
-        .returning('*')
-        .insert({
-          name: profile.name,
-          email: profile.email,
-        });
-      console.log('new user', user);
-    }
-    return user;
-  } catch (error) {
-    console.log('err', error);
-    return { error };
-  }
-};
-
 const createJwt = (profile) => {
   return JsonWebToken.sign(profile, JWT_KEY, {
     expiresIn: '2d',
@@ -59,23 +35,37 @@ const formatResponse = (jwt, user) => {
   };
 };
 
+/* POST auth */
 router.post('/', async (req, res) => {
   try {
     const query = createQueryParam(req.body.socialToken);
     const url = `https://graph.facebook.com/me?${query}`;
     const profile = await (await fetch(url)).json();
     console.log('profile', profile);
-    // TODO add error handling for error object from fb graph.
-
-    const user = await saveNewUser(profile);
-    const { error } = user;
-    if (error) {
-      // TODO handle error properly
-      throw new TypeError('Internal Server Error.');
+    // error handling for fb graph
+    if (profile.error) {
+      sendResponse(res, RES_STAT.BAD_REQ.CODE, profile.error);
+      return;
     }
 
+    // get user
+    const { name, email } = profile;
+    let [user] = await db('user')
+      .where({ email })
+      .select();
+    console.log('user', user);
+
+    if (!user) {
+      [user] = await db('user')
+        .insert({ name, email })
+        .returning('*');
+      console.log('new user', user);
+    }
+
+    // create jwt
     const jwt = createJwt(profile);
     console.log('jwt', jwt);
+
     sendResponse(res, RES_STAT.OK.CODE, formatResponse(jwt, user));
   } catch (err) {
     console.log('err', err);
