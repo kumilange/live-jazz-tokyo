c2def2b6514a61ff31cb84370ed221c5fac7764e
Kumiko Haraguchi <email.935@gmail.com>
2018-01-11 13:08:33 +0900

    Add userid to event table

---
+++ migrations/201801111235_add_userid_to_event.js
@@ -0,0 +1,12 @@
+exports.up = (knex) => {
+  return knex.schema.table('event', (t) => {
+    return t.integer('user_id')
+      .references('user.id');
+  });
+};
+
+exports.down = (knex) => {
+  return knex.schema.table('event', (t) => {
+    return t.dropColmn('user_id');
+  });
+};
