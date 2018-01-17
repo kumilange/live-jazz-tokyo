d44a0a663f9bba7d452c8bfe758bd5e0f648eba7
Kumiko Haraguchi <email.935@gmail.com>
2018-01-17 12:14:11 +0900

    Change Event component to dumb

--- src/containers/organisms/Event.js
+++ src/containers/organisms/Event.js
@@ -1,8 +1,29 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 
 import Event from '../../components/organisms/Event';
 import { getEventDetails, toggleMap } from '../../actions';
+
+class EventContainer extends Component {
+  componentDidMount() {
+    this.props.getEventDetails(this.props.match.params.id);
+  }
+
+  render() {
+    return (
+      <Event
+        {...this.props}
+      />
+    );
+  }
+}
+
+EventContainer.propTypes = {
+  match: PropTypes.shape().isRequired,
+  getEventDetails: PropTypes.func.isRequired,
+};
 
 const mapStateToProps = state => ({
   event: state.event.eventDetails,
@@ -15,4 +36,4 @@ const mapDispatchToProps = dispatch => bindActionCreators({
   getEventDetails, toggleMap,
 }, dispatch);
 
-export default connect(mapStateToProps, mapDispatchToProps)(Event);
+export default connect(mapStateToProps, mapDispatchToProps)(EventContainer);
