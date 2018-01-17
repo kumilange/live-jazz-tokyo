48d3ceaf6aeb63da29181d18e03ee00391886912
Kumiko Haraguchi <email.935@gmail.com>
2018-01-17 19:58:06 +0900

    Adjust Loader appearance

--- src/components/organisms/Event/index.js
+++ src/components/organisms/Event/index.js
@@ -12,8 +12,8 @@ import { ClockIcon, YenIcon, PinIcon } from '../../atoms/Icon/Icon';
 const FALLBACK_DESC = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
 
 const Event = ({ eventDetails, jwt, isFetching, toggleMap, showMap }) => {
-  if (isObjectEmpty(eventDetails)) return null;
   if (isFetching) return <Loader />;
+  if (isObjectEmpty(eventDetails)) return null;
   return (
     <main className="restrict-width grow" id="event-details">
       <div className="image-box flex center">
