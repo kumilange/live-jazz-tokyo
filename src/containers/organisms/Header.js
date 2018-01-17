afb1d98fec91bc08c0a40828ab9e9fd83af5a1c5
Kumiko Haraguchi <email.935@gmail.com>
2018-01-17 15:37:01 +0900

    Fix proptypes add isRequred Header

--- src/containers/organisms/Header.js
+++ src/containers/organisms/Header.js
@@ -1,8 +1,34 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 
+import hello from '../../config/hello';
 import { addAuthListener, logout } from '../../actions';
 import Header from '../../components/organisms/Header';
+
+class HeaderContainer extends Component {
+  componentDidMount() {
+    this.props.addAuthListener();
+    this.login = this.login.bind(this);
+  }
+
+  async login() {
+    await hello('facebook')
+      .login({ scope: 'email' });
+  }
+
+  render() {
+    return (<Header
+      {...this.props}
+      login={this.login}
+    />);
+  }
+}
+
+HeaderContainer.propTypes = {
+  addAuthListener: PropTypes.func.isRequired,
+};
 
 const mapStateToProps = state => ({
   userProfile: state.user.userProfile,
@@ -12,4 +38,4 @@ const mapDispatchToProps = dispatch => bindActionCreators({
   addAuthListener, logout,
 }, dispatch);
 
-export default connect(mapStateToProps, mapDispatchToProps)(Header);
+export default connect(mapStateToProps, mapDispatchToProps)(HeaderContainer);
