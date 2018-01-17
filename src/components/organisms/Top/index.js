9e2ead8e3ba5b620f447211f897708751ed1aa39
Kumiko Haraguchi <email.935@gmail.com>
2018-01-17 16:34:35 +0900

    Fix proptypes add isRequred Top

--- src/components/organisms/Top/index.js
+++ src/components/organisms/Top/index.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import { Marker } from 'react-google-maps';
 
@@ -6,47 +6,31 @@ import Map from '../../molecules/Map';
 import MyMarkerList from '../../molecules/MarkerList';
 import { isObjectEmpty } from '../../../utils';
 
-class Top extends Component {
-  componentDidMount() {
-    if (!navigator.geolocation) {
-      alert('Geolocation is not supported by your browser');
-      return;
-    }
-    navigator.geolocation.getCurrentPosition((position) => {
-      this.props.setUserLocation({
-        lat: position.coords.latitude,
-        lng: position.coords.longitude,
-      });
-    });
-    this.props.initializeEvents();
-  }
-
-  render() {
-    const { events, selectedEvent, userLocation, setSelectedEvent, clearSelectedEvent } = this.props;
-    return process.env.npm_lifecycle_event === 'test'
-      ? <div />
-      : <main id="top" className="restrict-width">
-        <Map
-          position={userLocation}
-          containerElement={<div style={{ height: '100%' }} />}
-          mapElement={<div style={{ height: '100%' }} />}
-        >
-          <div className="mapLable">On Tonight</div>
-          <MyMarkerList
-            events={events}
-            selectedEvent={selectedEvent}
-            setSelectedEvent={setSelectedEvent}
-            clearSelectedEvent={clearSelectedEvent}
-          />
-          {!isObjectEmpty(userLocation) ?
-            <Marker
-              position={userLocation}
-              icon={userLocation}
-            /> : null}
-        </Map>
-      </main>;
-  }
-}
+const Top = (props) => {
+  const { events, selectedEvent, userLocation, setSelectedEvent, clearSelectedEvent } = props;
+  return process.env.npm_lifecycle_event === 'test'
+    ? <div />
+    : <main id="top" className="restrict-width">
+      <Map
+        position={userLocation}
+        containerElement={<div style={{ height: '100%' }} />}
+        mapElement={<div style={{ height: '100%' }} />}
+      >
+        <div className="mapLable">On Tonight</div>
+        <MyMarkerList
+          events={events}
+          selectedEvent={selectedEvent}
+          setSelectedEvent={setSelectedEvent}
+          clearSelectedEvent={clearSelectedEvent}
+        />
+        {!isObjectEmpty(userLocation) ?
+          <Marker
+            position={userLocation}
+            icon={userLocation}
+          /> : null}
+      </Map>
+    </main>;
+};
 
 Top.propTypes = {
   events: PropTypes.arrayOf(PropTypes.object).isRequired,
@@ -54,8 +38,6 @@ Top.propTypes = {
   userLocation: PropTypes.shape().isRequired,
   setSelectedEvent: PropTypes.func.isRequired,
   clearSelectedEvent: PropTypes.func.isRequired,
-  setUserLocation: PropTypes.func.isRequired,
-  initializeEvents: PropTypes.func.isRequired,
 };
 
 export default Top;
--- src/containers/organisms/Top.js
+++ src/containers/organisms/Top.js
@@ -1,8 +1,37 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 
 import { initializeEvents, setSelectedEvent, clearSelectedEvent, setUserLocation } from '../../actions';
 import Top from '../../components/organisms/Top';
+
+class TopContainer extends Component {
+  componentDidMount() {
+    if (!navigator.geolocation) {
+      alert('Geolocation is not supported by your browser');
+      return;
+    }
+    navigator.geolocation.getCurrentPosition((position) => {
+      this.props.setUserLocation({
+        lat: position.coords.latitude,
+        lng: position.coords.longitude,
+      });
+    });
+    this.props.initializeEvents();
+  }
+
+  render() {
+    return (
+      <Top {...this.props} />
+    );
+  }
+}
+
+TopContainer.propTypes = {
+  setUserLocation: PropTypes.func.isRequired,
+  initializeEvents: PropTypes.func.isRequired,
+};
 
 const mapStateToProps = state => ({
   events: state.event.events,
@@ -14,5 +43,5 @@ const mapDispatchToProps = dispatch => bindActionCreators({
   initializeEvents, setSelectedEvent, clearSelectedEvent, setUserLocation,
 }, dispatch);
 
-export default connect(mapStateToProps, mapDispatchToProps)(Top);
+export default connect(mapStateToProps, mapDispatchToProps)(TopContainer);
 
