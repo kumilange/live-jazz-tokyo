77ae3b647e4dd4ab7dd26f836fa464be22470b30
Kumiko Haraguchi <email.935@gmail.com>
2018-02-13 23:43:30 +0900

    Add animation for routing

--- src/components/templates/App/index.js
+++ src/components/templates/App/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
+import { TransitionGroup, CSSTransition } from 'react-transition-group';
 
 import Header from '../../../containers/organisms/Header';
 import Event from '../../../containers/organisms/Event';
@@ -13,17 +14,28 @@ import './App.css';
 
 const App = () => (
   <BrowserRouter>
-    <div id="app">
-      <Header />
-      <Route exact path="/" component={Top} />
-      <Route exact path="/event/:id" component={Event} />
-      <Route exact path="/user/:id" component={User} />
-      <Route exact path="/pay" component={Pay} />
-      <Route exact path="/confirmation" component={Confirmation} />
-      <Route exact path="/addevent" component={AddEvent} />
-      <Footer />
-    </div>
-  </BrowserRouter>);
+    <Route render={({ location }) =>
+      (<TransitionGroup>
+        <CSSTransition
+          key={location.key}
+          timeout={300}
+          classNames="fade"
+        >
+          <div id="app">
+            <Header />
+            <Route exact path="/" component={Top} />
+            <Route exact path="/event/:id" component={Event} />
+            <Route exact path="/user/:id" component={User} />
+            <Route exact path="/pay" component={Pay} />
+            <Route exact path="/confirmation" component={Confirmation} />
+            <Route exact path="/addevent" component={AddEvent} />
+            <Footer />
+          </div>
+        </CSSTransition>
+      </TransitionGroup>)}
+    />
+  </BrowserRouter>
+);
 
 export default App;
 
